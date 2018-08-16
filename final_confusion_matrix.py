329012f01e43e47c38375108024ded78013c6963
knight <8189513+sherlockzoom@users.noreply.github.com>
2018-08-16 10:14:42 +0800

    Update final_confusion_matrix.py

--- final_confusion_matrix.py
+++ final_confusion_matrix.py
@@ -2,8 +2,6 @@ import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 
-from sklearn import svm, datasets
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
 # import some data to play with
